de7ad8a281fa1ffc501f555433ac879864428024
Dylann Vandenbussche <vandenbussche.dylann@gmail.com>
2020-12-18 13:23:59 +0100

    Endpoints Actuator

--- src/main/java/com/openclassrooms/safetynetalerts/SafetynetAlertsApplication.java
+++ src/main/java/com/openclassrooms/safetynetalerts/SafetynetAlertsApplication.java
@@ -1,13 +1,21 @@
 package com.openclassrooms.safetynetalerts;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SafetynetAlertsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SafetynetAlertsApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(SafetynetAlertsApplication.class, args);
+    }
+
+    @Bean
+    protected HttpTraceRepository httpTraceRepository() {
+	return new InMemoryHttpTraceRepository();
+    }
 
 }
